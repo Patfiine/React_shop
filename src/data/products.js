6ddccee22ea150093115bec373ef27a1f0007326
Илья Ананьев <ilaananev@MacBook-Pro-Ila.local>
2025-11-15 21:26:14 +0300

    Ничего

--- src/data/products.js
+++ src/data/products.js
@@ -2,6 +2,7 @@
 const products = [
   {
     id: 1,
+    categoryId: "macbook-pro",
     title: "Macbook Pro 14 ",
     price: 3500,
     description: "Процессор m3, 8/512",
@@ -10,6 +11,7 @@ const products = [
   
   {
     id: 2,
+    categoryId: "macbook-pro",
     title: "Macbook Pro 14",
     price: 3700,
     description: "Процессор m3 8/1000",
@@ -17,6 +19,7 @@ const products = [
   },
   {
     id: 3,
+    categoryId: "macbook-pro",
     title: "Macbook Pro 14",
     price: 4000,
     description: "Процессор m4 16/512",
@@ -24,6 +27,7 @@ const products = [
   },
   {
     id: 4,
+    categoryId: "macbook-pro",
     title: "Macbook Pro 14",
     price: 4000,
     description: "Процессор m1max 32/512",
@@ -31,6 +35,7 @@ const products = [
   },
   {
     id: 5,
+    categoryId: "macbook-pro",
     title: "Macbook Pro 14",
     price: 4300,
     description: "Процессор m1max 32/1000",
@@ -38,6 +43,7 @@ const products = [
   },
    {
     id: 6,
+    categoryId: "macbook-pro",
     title: "Macbook Pro 14",
     price: 4600,
     description: "Процессор m3pro 18/512",
@@ -45,6 +51,7 @@ const products = [
   },
   {
     id: 7,
+    categoryId: "macbook-pro",
     title: "Macbook Pro 16",
     price: 4750,
     description: "Процессор m1max 32/1024",
@@ -52,6 +59,7 @@ const products = [
   },
   {
     id: 8,
+    categoryId: "macbook-pro",
     title: "Macbook Pro 16",
     price: 5300,
     description: "Процессор m3pro 36/512",
@@ -59,6 +67,7 @@ const products = [
   },
   {
     id: 9,
+    categoryId: "macbook-pro",
     title: "Macbook Pro 16",
     price: 6500,
     description: "Процессор m4pro 24/512",
@@ -66,6 +75,7 @@ const products = [
   },
   {
     id: 10,
+     categoryId: "macbook-pro",
     title: "Macbook Pro 16",
     price: 4000,
     description: "Процессор m1 max 32/1024",
@@ -73,18 +83,116 @@ const products = [
   },
   {
     id: 11,
-    title: "Macbook air 15",
+    categoryId: "macbook-air",
+    title: "Macbook Air 15",
     price: 3450,
     description: "Процессор m4 16/256",
     image: "/images/air.jpeg"
   },
    {
     id: 12,
-    title: "Macbook air 13",
+    categoryId: "macbook-air",
+    title: "Macbook Air 13",
     price: 3000,
     description: "Процессор m4 16/256",
     image: "/images/air.jpeg"
-  }
+  },
+   {
+    id: 13,
+    categoryId: "iphone-base",
+    title: "Iphone base 17",
+    price: 3000,
+    description: "256 GB",
+    image: "/images/iphone.png"
+  },
+  {
+    id: 14,
+    categoryId: "iphone-base",
+    title: "Iphone 17",
+    price: 2680,
+    description: "512 GB",
+    image: "/images/iphone.png"
+  },
+    {
+    id: 15,
+    categoryId: "iphone-base",
+    title: "Iphone 16",
+    price: 2000,
+    description: "256 GB",
+    image: "/images/iphone.png"
+  },
+    {
+    id: 16,
+    categoryId: "iphone-base",
+    title: "Iphone 16",
+    price: 1900,
+    description: "128 GB",
+    image: "/images/iphone.png"
+  },
+    {
+    id: 17,
+    categoryId: "iphone-base",
+    title: "Iphone 16",
+    price: 1850,
+    description: "128 GB",
+    image: "/images/iphone.png"
+  },
+    {
+    id: 18,
+    categoryId: "iphone-base",
+    title: "Iphone 16",
+    price: 2200,
+    description: "256 GB",
+    image: "/images/iphone.png"
+  },
+   {
+    id: 19,
+    categoryId: "iphone-pro",
+    title: "Iphone 16 Pro",
+    price: 3500,
+    description: "512 GB",
+    image: "/images/iphone pro.png"
+  },
+   {
+    id: 20,
+    categoryId: "iphone-pro",
+    title: "Iphone 17 Pro",
+    price: 4150,
+    description: "512 GB",
+    image: "/images/iphone pro.png"
+  },
+   {
+    id: 21,
+    categoryId: "iphone-pro",
+    title: "Iphone 16 Pro",
+    price: 3650,
+    description: "256 GB",
+    image: "/images/iphone pro.png"
+  },
+   {
+    id: 22,
+    categoryId: "iphone-pro",
+    title: "Iphone 17 Pro",
+    price: 4200,
+    description: "256 GB",
+    image: "/images/iphone pro.png"
+  },
+   {
+    id: 23,
+    categoryId: "iphone-pro",
+    title: "Iphone 15 Pro",
+    price: 2500,
+    description: "512 GB",
+    image: "/images/iphone pro.png"
+  },
+   {
+    id: 15,
+    categoryId: "iphone-pro",
+    title: "Iphone 17 Pro",
+    price: 4350,
+    description: "512 GB",
+    image: "/images/iphone pro.png"
+  },
 ];
 
 export default products;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -32,15 +32,16 @@ export default function Shop() {
   const [selectedCategory, setSelectedCategory] = useState(null);
 
   const categories = [
-    { id: 'pro', title: 'MacBook Pro', image: '/images/m3 silver.jpeg' },
-    { id: 'air', title: 'MacBook Air', image: '/images/air.jpeg' },
-    { id: 'Iphone', title: 'Iphone', image: '/images/iphone.png' },
-    { id: 'Iphone Pro', title: 'Iphone Pro', image: '/images/iphone pro.png' } 
+    { id: 'macbook-pro', title: 'MacBook Pro', image: '/images/m3 silver.jpeg' },
+    { id: 'macbook-air', title: 'MacBook Air', image: '/images/air.jpeg' },
+    { id: 'iphone-base', title: 'Iphone', image: '/images/iphone.png' },
+     { id: 'iphone-pro', title: 'Iphone Pro', image: '/images/iphone pro.png' },
+    
   ];
 
   const filteredProducts = selectedCategory
-    ? products.filter(p => p.title.toLowerCase().includes(selectedCategory))
-    : [];
+  ? products.filter(p => p.categoryId === selectedCategory)
+  : [];
 
   return (
     <main style={{padding: 20}}>
